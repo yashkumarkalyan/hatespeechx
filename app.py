59bc07a4d7abfd29ce2042df9264b0759a9768a7
warrior <82755153+yashkumarkalyan@users.noreply.github.com>
2023-02-24 23:16:35 +0530

    finally run

--- app.py
+++ app.py
@@ -4,24 +4,27 @@ import numpy as np
 import pickle
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.svm import LinearSVC
+from joblib import Parallel, delayed
+import joblib
 # Load the trained model and vectorizer
-model = pickle.load(open("nunu.pkl", "rb"))
-vectorizer = pickle.load(open("chunu.pkl", "rb"))
+model = joblib.load(open("clf.pkl", "rb"))
+vectorizer = joblib.load(open("cv.pkl", "rb"))
 app = Flask(__name__)
 
-@app.route('/predict', methods=['POST'])
+@app.route('/predict', methods=['GET','POST'])
 def predict():
     # Get the text input from the API request
-    text = "Hkko there killer"
+    text = "Bitch Fucker MOtherfucker racist"
+
 
     # Convert the text input to a vector using the trained vectorizer
-    vector = vectorizer.transform([text])
+    vector = vectorizer.transform([text]).toarray()
 
     # Use the trained model to make a prediction
-    prediction = model.predict(vector)[0]
-
+    prediction = model.predict(vector)
+    print(prediction[0])
     # Return the prediction as a JSON response
-    response = {'prediction': prediction}
-    return jsonify(response)
+    # response = {'prediction': prediction}
+    return ("Answer is " + str(prediction[0]))
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
